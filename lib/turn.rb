3ba8930eabf22f88be58247db215fca9cfcd5869
Alora Riley <alora.riley@gmail.com>
2024-08-22 16:07:44 -0600

    Finish turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,4 +8,18 @@ class Turn
     end
 
     def correct?
+        if @guess == card.answer
+            return true
+        else
+            false
+        end
+    end
+
+    def feedback 
+        if @guess == card.answer
+            "Correct!"
+        else 
+            "Incorrect."
+        end
+    end
 end

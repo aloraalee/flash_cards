620218e365ba1c796980cfa9044edfcd3516a2f0
Alora Riley <alora.riley@gmail.com>
2024-08-22 16:08:21 -0600

    Finish turn test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -20,6 +20,27 @@ RSpec.describe Turn do
         card = Card.new("What is the capital of Alaska", "Juneau", :Geography)
         turn = Turn.new("Juneau", card)
 
-        expect(turn.correct?). to eq(true)
+        expect(turn.correct?).to eq(true)
+    end
+
+    it 'had an incorrect guess' do
+        card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+        turn = Turn.new("Saturn", card)
+
+        expect(turn.correct?).to eq(false)
+    end
+
+    it 'gives feedback when correct' do
+        card = Card.new("What is the capital of Alaska", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.feedback).to eq("Correct!")
+    end
+       
+    it 'gives feedback when incorrect' do
+        card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+        turn = Turn.new("Saturn", card)
+
+        expect(turn.feedback).to eq("Incorrect.")
     end
 end

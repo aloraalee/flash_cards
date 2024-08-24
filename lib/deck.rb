ab6ff0e44b63b4c1edaa8701e8523ff773743693
Alora Riley <alora.riley@gmail.com>
2024-08-23 20:04:56 -0600

    Add count, attempt cards in category

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,13 +1,24 @@
 class Deck
     attr_reader :cards
-    
+     attr_accessor :card
+
     def initialize(cards)
         @cards = cards
-    
     end
     
-    def cards
-        []
+    def count
+        cards.length
+    end
+
+    def cards_in_category(category_type)
+        @cards.find_all do |card|
+            # @card = card
+            category_type = []
+            card.category == category_type
+            category_type << card
+
+        end
+
     end
 end
 

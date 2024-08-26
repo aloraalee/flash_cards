0fa33e1cc04d708fa95a2b61f63e2fbee48affe5
Alora Riley <alora.riley@gmail.com>
2024-08-25 22:57:31 -0600

    Add number correct by category method that passes test

--- lib/round.rb
+++ lib/round.rb
@@ -1,6 +1,6 @@
 class Round
     attr_accessor :deck, :cards, :card, :turn, 
-                :current_card, :turn
+                :current_card, :category, :turns
 
     def initialize(deck)
         @deck = deck
@@ -9,32 +9,37 @@ class Round
         @card = card
         @turn = turn
         @current_card = @deck.cards.first
+        @correct_count = 0
     end
-
-    def turns
-        []
-    end
-
-    def new_turn = take_turn
 
     def take_turn(guess)
         turn = Turn.new(guess, @current_card)
         @turns << turn
-        # require 'pry'; binding.pry
-        # move_to_next_card
-       return turn
-       turns << turn
+            if turn.correct? == true
+                @correct_count += 1
+            end
+        move_to_next_card
+        turn
        
     end
 
-    
+    def move_to_next_card
+        current_index = @deck.cards.index(@current_card)
+        if current_index + 1 < @deck.cards.length
+        @current_card = @deck.cards[current_index + 1] 
+        end
+    end 
 
+    def number_correct
+        @correct_count
+    end
 
-    # def move_to_next_card
-    #     current_index = @deck.cards.index(@current_card)
-    #     if current_index += 1 < @deck.cards.length
-    #     @current_card = @deck.cards[current_index + 1] 
-    #     end
-    # end 
-        
+    def number_correct_by_category(category)
+        @turns.count { |turn| turn.correct? && turn.card.category == category }
+    end
+
+    def percent_correct 
+        (number_correct / turns.count) * 100
+    end
+
 end

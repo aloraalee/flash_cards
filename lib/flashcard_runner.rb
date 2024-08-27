ba2be4308c90ad3d28f2544582d0f72c7dbf1ca8
Alora Riley <alora.riley@gmail.com>
2024-08-26 19:39:39 -0600

    Initial Commit

---
+++ lib/flashcard_runner.rb
@@ -0,0 +1,9 @@
+# Create some Cards
+# Put those card into a Deck
+# Create a new Round using the Deck you created
+# Start the round using a new method called start
+
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'

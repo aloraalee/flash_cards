cf92a40ae9323e8e276f5f3bfcc49649f88ccd5a
Alora Riley <alora.riley@gmail.com>
2024-08-28 10:09:36 -0600

    Delete runner

--- lib/runner.rb
+++
@@ -1,6 +0,0 @@
-require './card'
-
-card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card2 = Card.new("What is the closest planet to the sun?", "Mercury", :STEM)
-
-require 'pry'; binding.pry
